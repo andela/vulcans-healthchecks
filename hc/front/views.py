77b1e6413797e37ae711431612c03c23c46f1ad2
eugenius <eugene.badriku@andela.com>
2017-06-26 10:51:02 +0300

    Modify failed_checks for correct status determination

--- hc/front/views.py
+++ hc/front/views.py
@@ -64,12 +64,12 @@ def my_checks(request):
 
 @login_required
 def failed_checks(request):
-    q = Check.objects.filter(user=request.team.user, status="down").order_by("created")
-    checks = list(q)
+    q = Check.objects.filter(user=request.team.user).order_by('created')
+    down_checks = [check for check in list(q) if check.get_status() == 'down']
 
     counter = Counter()
     down_tags, grace_tags = set(), set()
-    for check in checks:
+    for check in down_checks:
         status = check.get_status()
         for tag in check.tags_list():
             if tag == "":
@@ -82,8 +82,7 @@ def failed_checks(request):
 
     ctx = {
         "page": "failing_checks",
-        "checks": checks,
-        "now": timezone.now(),
+        "checks": down_checks,
         "tags": counter.most_common(),
         "down_tags": down_tags,
         "grace_tags": grace_tags,
