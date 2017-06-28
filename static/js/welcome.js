c8deada5b9d6bcc2b000513f659948525c4ccb4a
Alweezy <alvinmutisya@gmail.com>
2017-06-28 09:53:11 +0300

    [#147181013] add slide show functionality

--- static/js/welcome.js
+++ static/js/welcome.js
@@ -6,3 +6,33 @@ setTimeout(function() {
     });
 
 }, 1000);
+
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(index) {
+  showSlides(slideIndex += index);
+}
+
+function currentSlide(index) {
+  showSlides(slideIndex = index);
+}
+
+function showSlides(index) {
+  var counter;
+  var slides = document.getElementsByClassName("slides");
+  var dots = document.getElementsByClassName("dot");
+  if (index > slides.length) {slideIndex = 1}
+  if (index < 1) {slideIndex = slides.length}
+  for (counter = 0; counter < slides.length; counter++) {
+      slides[counter].style.display = "none";
+  }
+  for (counter = 0; counter < dots.length; counter++) {
+      dots[counter].className = dots[counter].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
+
+
