bfe19fe937666f92080a6cfd7a8d4b3bc848819b
Akash <akashbaga011@gmail.com>
2017-07-05 11:32:40 +0300

    Ammended requirements

--- Procfile
+++ Procfile
@@ -1,1 +1,2 @@
-web: gunicorn hc.wsgi
+web: gunicorn hc.wsgi --log-file -
+release: python manage.py migrate
--- hc/settings.py
+++ hc/settings.py
@@ -154,16 +154,3 @@ if os.path.exists(os.path.join(BASE_DIR, "hc/local_settings.py")):
     from .local_settings import *
 else:
     warnings.warn("local_settings.py not found, using defaults")
-import dj_database_url
-DATABASES['default'] = dj_database_url.config()
-
-SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-
-ALLOWED_HOSTS = ['*']
-
-DEBUG = False
-
-try:
-    from .local_settings import *
-except ImportError:
-    pass
