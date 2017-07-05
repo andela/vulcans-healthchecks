a4206fd0a328ec8a88e5278f55e848ef2fb10284
Ndagi Stanley <stanley.ndagi@andela.com>
2017-07-05 15:53:47 +0300

    [Chore] Setup Staging/ Production deployment

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: python manage.py makemigrations && python manage.py migrate && gunicorn hc.wsgi --log-file -
--- hc/settings.py
+++ hc/settings.py
@@ -16,9 +16,9 @@ import warnings
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 HOST = "localhost"
-SECRET_KEY = "---"
+SECRET_KEY = os.getenv('SECRET_KEY')
 DEBUG = True
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 DEFAULT_FROM_EMAIL = 'healthchecks@example.org'
 USE_PAYMENTS = False
 
@@ -109,6 +109,10 @@ if os.environ.get("DB") == "mysql":
             'TEST': {'CHARSET': 'UTF8'}
         }
     }
+
+# Update database configuration with $DATABASE_URL.
+if os.environ.get("HOST") == "HEROKU":
+    DATABASES['default'] = dj_database_url.config()
 
 LANGUAGE_CODE = 'en-us'
 
