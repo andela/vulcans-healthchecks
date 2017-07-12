0dbaa8695d30376a4a5c9458f3819b8c19ab6c2a
eugenius <eugene.badriku@andela.com>
2017-07-12 18:01:26 +0300

    Reset settings.py

--- hc/settings.py
+++ hc/settings.py
@@ -43,7 +43,6 @@ INSTALLED_APPS = (
 
 MIDDLEWARE = (
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -51,7 +50,6 @@ MIDDLEWARE = (
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'hc.accounts.middleware.TeamAccessMiddleware',
-
 )
 
 AUTHENTICATION_BACKENDS = (
