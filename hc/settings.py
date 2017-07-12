49e2c338c171809e8bf67544f75e98a0b8401d0a
eugenius <eugene.badriku@andela.com>
2017-07-12 15:13:55 +0300

    Shift whitenoise middleware higher

--- hc/settings.py
+++ hc/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = (
 
 MIDDLEWARE = (
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -50,7 +51,7 @@ MIDDLEWARE = (
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'hc.accounts.middleware.TeamAccessMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
+
 )
 
 AUTHENTICATION_BACKENDS = (
